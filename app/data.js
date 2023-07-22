4b2d7d62eef39437c79c8f32086f1e9c96edb905
pro-mannan <promasudbd@gmail.com>
2023-07-22 23:02:28 +0600

    team member dynamic data using here now

---
+++ app/data.js
@@ -0,0 +1,26 @@
+const members = [
+    {
+        id: 1,
+        postImg: "../assets/images/1.jpeg",
+        content: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, nesciunt!",
+        link: 200,
+        comment: 20,
+        timeAgo: "20",
+        author: {
+            name: "Roksana Islam",
+            photo: "../assets/images/rana.png",
+        },
+    },
+    {
+        id: 1,
+        postImg: "../assets/images/1.jpeg",
+        content: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, nesciunt!",
+        link: 200,
+        comment: 20,
+        timeAgo: "20",
+        author: {
+            name: "Masud Rana",
+            photo: "../assets/images/rana.png",
+        },
+    }
+]
